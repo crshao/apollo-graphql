a4cd3121944be95f84b9ef2242313c9062924ee2
Calvin Wijaya <47976609+crshao@users.noreply.github.com>
2021-07-15 16:56:38 +0700

    connecting earthquakes datasources and create quakes and quake endpoint

---
+++ server/resolvers.js
@@ -0,0 +1,10 @@
+module.exports = {
+    Query: {
+      quakes: (_, __, { dataSources }) =>
+        dataSources.quakeAPI.getAllQuakes(),
+      quake: (_, { id }, { dataSources }) =>
+        dataSources.quakeAPI.getQuakeById({ quakeId: id }),
+    //   me: (_, __, { dataSources }) => dataSources.userAPI.findOrCreateUser()
+    }
+  };
+  
