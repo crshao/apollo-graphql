7f5af092ad0d0fce5c20ec04987d3ac9b5a6ee41
Calvin Wijaya <47976609+crshao@users.noreply.github.com>
2021-07-28 22:52:34 +0700

    add authentication using jwt

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
+const jwt = require('jsonwebtoken');
 const { createStore } = require('./utils');
 
 const QuakeAPI = require('./datasources/quake');
@@ -12,8 +13,26 @@ const server = new ApolloServer({
     context: async ({ req }) => {
         // simple auth check on every request
         const auth = req.headers && req.headers.authorization || '';
-        const email = Buffer.from(auth, 'base64').toString('ascii');
+        let email = "";
+        let token = "";
         
+        const getToken = () => {
+            return auth.split(" ")[1];
+        }
+
+        if (auth.length && auth.split(" ")[1])
+        {
+            token = getToken();
+        }
+
+        if (token !== "")
+        {
+            email = jwt.verify(token, "secret_key").email;
+        }
+
+
+
+
         // find a user by their email
         const usercheck = await store.users.map( user => {
             if (email === user.email) {
