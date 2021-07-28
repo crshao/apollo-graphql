113560e2133c18d18aab9f61f499d427619c224d
Calvin Wijaya <47976609+crshao@users.noreply.github.com>
2021-07-28 16:15:02 +0700

    add authentication

--- server/datasources/user.js
+++ server/datasources/user.js
@@ -14,6 +14,52 @@ class UserAPI extends DataSource {
         const users = this.store.users
         return users
     }
+
+    async getUser({email: emailArg}) {
+        let index = 0;
+
+        const email = this.context && this.user ?
+        this.context.user.email : emailArg;
+
+        const theUser = this.store.users.map(user => {
+            if (email === user.email) {
+                index = this.store.users.indexOf(user);
+                return user
+            }
+        });
+        return theUser[index];
+    }
+
+    async saveRecord({recordId}) {
+        const userId = this.context.user.id;
+
+        if (!userId)
+        {
+            console.log("No user on context");
+        }
+        else
+        {
+            console.log("User on context");
+        }
+
+        const usercheck = this.store.users.map(user => {
+            if (userId == user.id)
+            {
+                user.records.push({id: recordId});
+                return user
+            }
+        });
+
+        let users = []
+        await usercheck.forEach(elem => {
+            if (elem)
+            {
+                users.push(elem);
+            }
+        });
+
+        return users[0].records.length > 4 ? users[0].records : "error";
+    }
 }
 
 module.exports = UserAPI
